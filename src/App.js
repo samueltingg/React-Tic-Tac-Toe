0b6cd1309b365125668d9a91b7d4f184d5d2545c
samueltingg <samuelting7@gmail.com>
2024-09-06 09:50:45 +0800

    Revert "feat: function tic-tac-toe game created without history of moves played"
    This reverts commit 53a8e0484bd9fdaf8176bcddd1c955f34f97da34.

--- src/App.js
+++ src/App.js
@@ -1,105 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'
 
-function Square({value, onSquareClick}) {
+function App() {
   return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
-
-// return X, O, or null
-function calculateWinner(squares) {
-  const winLines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  // for each winLine check if they have the same value
-  for (let i = 0; i < winLines.length; i++) {
-    const [a, b, c] = winLines[i]; // destructuring
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-function Board({xIsNext, squares, onPlay}) {
-
-  // const [xIsNext, setXIsNext] = useState(true);
-  // const [squares, setSquares] = useState(Array(9).fill(null)); // sets var "squares" to an array with 9 elements, all with value of "null"
-  
-  const winner = calculateWinner(squares);
-  let status = 'Next Player is X';
-  if (winner) {
-    status = 'Winner is ' + winner; 
-  } else {
-    status = "Next Player: " + (xIsNext ? 'X' : 'O');
-  }
-
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const newSquares = squares.slice();
-    if (xIsNext) {
-      newSquares[i] = "X";
-    } else {
-      newSquares[i] = "O";
-    }
-    // setSquares(newSquares);
-    // setXIsNext(!xIsNext);
-    onPlay(newSquares);
-  }
-  return (
-    <>
-      <div>{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
-    </>
-  );
-}
-
-function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquare = history[history.length - 1];
-
-  function handlePlay(newSquares) {
-    setHistory([...history, newSquares]);
-    setXIsNext(!xIsNext);
-  }
-
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay}/>
-      </div>
-      <div className="game-info">
-        <ol>{/*TODO*/}</ol>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-export default Game;
+export default App;
